383ef2705709017bd650fb8396b724e9f97eabee
TeoLeclair <142941949+TeoLeclair@users.noreply.github.com>
2023-12-01 08:50:54 -0800

    First_Push

---
+++ main.py
@@ -0,0 +1,31 @@
+#this file was created by: Teo Leclair
+# source: python library
+
+# Title:
+# Rummy
+
+# Overview: 
+# create basic impleation of card game rummy
+
+# Goals:
+# Game Design
+# -card images
+# -buttons
+# Code game logic
+# - shuffling
+# - dealing
+# - turns
+# - scoring
+# - winning
+# Interaction
+# - how should player interact with cards, deck, etc
+# Opponent
+# - implement turn based 2 player or simple AI opponent
+# Game loop
+# - update game, redraw
+
+
+import tkinter as tk
+from tkinter import messagebox
+import random
+
