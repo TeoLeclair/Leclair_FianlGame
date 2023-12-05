961eb688c5f7750954c7d738117c3b7c0baf6f76
TeoLeclair <teolittleman@gmail.com>
2023-12-05 14:27:32 -0800

    README.md

--- final game/main.py
+++ final game/main.py
@@ -1,5 +1,10 @@
 #this file was created by: Teo Leclair
-# source: python library
+# source: 
+# python library
+# Cozort
+# Joseph
+# Chris Bradfield
+# ChatGPT
 
 # Title:
 # Rummy
@@ -24,9 +29,57 @@
 # Game loop
 # - update game, redraw
 
-# tank you sor
 
 import tkinter as tk
 from tkinter import messagebox
 import random
+class Rummy:
+    def create_board(self):
+        for i in range(4):
+            for j in range(4):
+                button = tk.Button(self.root, text=" ", width=5, height=2, command=lambda i=i, j=j: self.flip_card(i, j))
+                button.grid(row=i, column=j)
+                self.buttons.append(button)
+    def create_deck():
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        return [f'{rank} of {suit}' for suit in suits for rank in ranks]
+    def shuffle_deck(deck):
+        random.shuffle(deck)
+        return deck
+def deal_cards(deck, num_cards):
+    return [deck.pop() for _ in range(num_cards)]
+
+def print_hand(hand):
+    print("Your hand:")
+    for card in hand:
+        print(card)
+
+def main():
+    deck = create_deck()
+    shuffled_deck = shuffle_deck(deck)
+
+    player_hands = {
+        'Player 1': deal_cards(shuffled_deck, 7),
+        'Player 2': deal_cards(shuffled_deck, 7)
+    }
+
+    current_player = 'Player 1'
+    while True:
+        print(f"\n{current_player}'s turn. Enter 'p' to print your hand or 'f' to finish your turn.")
+        action = input("Enter action: ").strip().lower()
+
+        if action == 'p':
+            print_hand(player_hands[current_player])
+        elif action == 'f':
+            current_player = 'Player 2' if current_player == 'Player 1' else 'Player 1'
+        elif action == 'q':
+            print("quitting game...")
+            exit()
+        else:
+            print("Invalid action. Please enter 'p' or 'f'.")
+
+if __name__ == "__main__":
+    main()
+
 
